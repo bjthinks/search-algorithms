8bb1e063c933499e9349bfb56d32dcbbe10b9a05
Brian Johnson <bjthinks@gmail.com>
2026-01-04 03:32:34 -0800

    Add some search algorithms and some tests on sliding block puzzles from long ago.

---
+++ app/Search.hs
@@ -0,0 +1,165 @@
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts  #-}
+
+{-
+Breadth first: each step consists of expanding the entire frontier,
+checking if a goal has been reached, and, if not, constructing a new
+frontier.  This can be done incrementally with a FIFO queue.
+Neighbors is a list of states and actions. Costs are implicitly 1.
+Graph variant checks new states against a list of previously visited
+states; tree variant does not.  Tree variant is not recommended on
+graphs, as the space savings of not storing the explored set are
+smaller than the cost to store the frontier, and the time costs of
+revisiting nodes are potentially very large.
+
+Uniform cost: each step consists of locating the node on the frontier
+with the lowest cost, checking if it is a goal, removing it from the
+frontier, expanding it, and adding its successors to the frontier.
+Neighbors is a list of states, actions, and costs.
+Graph variant checks new states against a list of previously visited
+states; tree variant does not.  Graph variant also needs to deal with
+the possibility that a node on the frontier may be generated at a
+later point in the search with a lower cost.  (The explored set is
+always generated in increasing order of cost.)  This is accomplished
+by applying the goal test when a state is selected for expansion, not
+when it is first generated.  As in breadth first search, the tree
+variant is not recommended on graphs.
+
+Depth first: each step consists of expanding the deepest unexpanded
+node, checking if a goal has been reached, and backing up only when
+there are no successors.  Graph variant keeps track of all visited
+states, but it never makes sense to use "depth first graph search"
+in this sense because breadth first is better.  "Tree" variant checks
+either nothing or nodes along the current history for duplication.
+Neighbors is a list of states and actions. Costs are implicitly 1.
+
+Closely related depth-limited search adds a depth limit, and informs
+the user if no solution exists, or if the cutoff was reached.
+
+Iterative deepening depth first: Repeat depth-limited search as long
+as the result is "cutoff reached", increasing depth limit by 1 each
+time.
+
+A*: an informed search; improvement on uniform cost search that also
+takes into account a lower bound on the cost from the current state
+to the goal.
+
+Iterative deepening A*: practical when step costs are unit.
+RBFS: see book... complicated...
+(Simplified) Memory-bounded A*... this is getting really complex...
+
+Bidirectional search: a form of breadth first search when the goal is
+known ahead of time, and the point of the search is only to find the
+shortest path.  Requires predecessors of a state to be computable
+(always true if state space is an undirected graph).  Can be done in
+any of the breadth first or uniform cost variants, including A*.
+-}
+
+module Search where
+
+import qualified Data.Set as S
+import qualified Data.Heap as H
+
+-- (Number of nodes expanded, Number of states stored at end of search)
+data Stats = S !Int !Int deriving Show
+
+breadthFirstTreeSearch :: (state -> [state]) -> state ->
+                          (state -> Bool) -> (Maybe [state],Stats)
+breadthFirstTreeSearch expand start isGoal
+  | isGoal start = (Just [start],S 0 0)
+  | otherwise = bfts [[start]] [] (S 0 0)
+    where
+      bfts [] [] stats = (Nothing,stats)
+      bfts [] new_frontier stats = bfts new_frontier [] stats
+      bfts (this_path:rest_of_frontier) new_frontier (S n s) =
+        let from_here = expand $ head this_path
+            goals_from_here = filter isGoal from_here
+        in if null goals_from_here
+           then bfts rest_of_frontier
+                (map (:this_path) from_here ++ new_frontier)
+                (S (n+1) (s-1+length from_here))
+           else (Just $ head goals_from_here : this_path,S (n+1) s)
+
+breadthFirstGraphSearch :: Ord state => (state -> [state]) -> state ->
+                           (state -> Bool) -> (Maybe [state],Stats)
+breadthFirstGraphSearch expand start isGoal
+  | isGoal start = (Just [start],S 0 0)
+  | otherwise = bfts [[start]] [] (S.singleton start) (S 0 0)
+    where
+      bfts [] [] _ stats = (Nothing,stats)
+      bfts [] new_frontier explored_set stats
+        = bfts new_frontier [] explored_set stats
+      bfts (this_path:rest_of_frontier) new_frontier explored_set (S n s) =
+        let from_here = filter (not . flip S.member explored_set) $
+                        expand $ head this_path
+            goals_from_here = filter isGoal from_here
+        in if null goals_from_here
+           then bfts rest_of_frontier
+                (map (:this_path) from_here ++ new_frontier)
+                (S.union explored_set $ S.fromList from_here)
+                (S (n+1) (s+length from_here))
+           else (Just $ head goals_from_here : this_path,S (n+1) s)
+
+uniformCostTreeSearch :: forall cost state. Real cost =>
+                         (state -> [(state,cost)]) -> state ->
+                         (state -> Bool) -> (Maybe ([state],cost),Stats)
+uniformCostTreeSearch expand start isGoal =
+  ucts (H.singleton (0,[start]) :: H.MinPrioHeap cost [state]) (S 0 0)
+    where
+      ucts h stats@(S n s) = case H.view h of
+        Nothing -> (Nothing,stats)
+        Just ((current_cost,current_path),h') ->
+          let current_state = head current_path
+              from_here = expand current_state
+          in if isGoal current_state
+             then (Just (current_path,current_cost),stats)
+             else ucts (H.union h' $ H.fromList $
+                  map (\(s,c) -> (current_cost+c,s:current_path)) $ from_here)
+                  (S (n+1) (s+length from_here))
+
+uniformCostGraphSearch :: forall cost state. (Real cost, Ord state) =>
+                         (state -> [(state,cost)]) -> state ->
+                         (state -> Bool) -> (Maybe ([state],cost),Stats)
+uniformCostGraphSearch expand start isGoal =
+  ucgs (H.singleton (0,[start]) :: H.MinPrioHeap cost [state])
+       (S.empty) (S 0 0)
+    where
+      ucgs h explored_set stats@(S n s) = case H.view h of
+        Nothing -> (Nothing,stats)
+        Just ((current_cost,current_path),h') ->
+          let current_state = head current_path
+              from_here = filter (not . flip S.member explored_set . fst) $
+                          expand current_state
+          in if isGoal current_state
+             then (Just (current_path,current_cost),stats)
+             else if S.member current_state explored_set
+                  then ucgs h' explored_set (S n (s-1))
+                  else ucgs (H.union h' $ H.fromList $
+                             map (\(s,c) -> (current_cost+c,s:current_path))
+                             from_here)
+                       (S.insert current_state explored_set)
+                       (S (n+1) (s-1+length from_here))
+
+aStarGraphSearch :: forall cost state. (Real cost, Ord state) =>
+                    (state -> [(state,cost)]) -> (state -> cost) -> state ->
+                    (state -> Bool) -> (Maybe ([state],cost),Stats)
+aStarGraphSearch expand lower_bound start isGoal =
+  ucgs (H.singleton (lower_bound start,(0,[start])) ::
+           H.MinPrioHeap cost (cost,[state]))
+       (S.empty) (S 0 0)
+    where
+      ucgs h explored_set stats@(S n s) = case H.view h of
+        Nothing -> (Nothing,stats)
+        Just ((_,(current_cost,current_path)),h') ->
+          let current_state = head current_path
+              from_here = filter (not . flip S.member explored_set . fst) $
+                          expand current_state
+          in if isGoal current_state
+             then (Just (current_path,current_cost),stats)
+             else if S.member current_state explored_set
+                  then ucgs h' explored_set (S n (s-1))
+                  else ucgs (H.union h' $ H.fromList $
+                             map (\(s,c) -> (current_cost+c+lower_bound s,
+                                             (current_cost+c,s:current_path)))
+                             from_here)
+                       (S.insert current_state explored_set)
+                       (S (n+1) (s-1+length from_here))
---
+++ app/SlidingBlockPuzzle.hs
@@ -0,0 +1,83 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Data.List (elemIndex)
+import Data.Array
+import Data.Maybe (fromJust)
+import Search
+
+data SlidingBlockPuzzle = SBP Int Int Int (Array Int (Array Int Int))
+                          deriving (Eq,Ord)
+
+instance Show SlidingBlockPuzzle where
+  show (SBP _ _ _ b) = ('\n':) $ unlines $
+                       map (unwords . map show . elems) (elems b)
+
+solvedSlidingBlockPuzzle :: Int -> SlidingBlockPuzzle
+solvedSlidingBlockPuzzle n
+  | n > 0 = SBP n 0 0 $
+            listArray (0,n-1) [listArray (0,n-1) [a..b] |
+                               (a,b) <- zip [0,n..n^2-n] [n-1,2*n-1..n^2-1]]
+  | otherwise = error "Sliding block puzzle of nonpositive size"
+
+expandSlidingBlockPuzzle :: SlidingBlockPuzzle -> [SlidingBlockPuzzle]
+expandSlidingBlockPuzzle p@(SBP n r c d)
+  = map swap $ filter valid $ [(r-1,c),(r+1,c),(r,c-1),(r,c+1)]
+  where
+    valid (x,y) = x >= 0 && x < n && y >= 0 && y < n
+    swap (x,y)
+      | x == r = SBP n x y $ d // [(r,(d!r) // [(y,0),(c,(d!r)!y)])]
+      | otherwise = SBP n x y $ d // [(r,(d!r) // [(c,(d!x)!c)]),
+                                      (x,(d!x) // [(c,(d!r)!c)])]
+
+misplacedDistance :: SlidingBlockPuzzle -> Int
+misplacedDistance (SBP n _ _ d)
+  = length $ filter ((/=0) . snd) $ filter (uncurry (/=)) $ zip [0..n^2-1] (concat $ map elems $ elems d)
+
+manhattanDistance :: SlidingBlockPuzzle -> Int
+manhattanDistance (SBP n _ _ arr)
+  = sum [l1distance (properLocation k) (actualLocation k) | k <- [1..n^2-1]]
+  where
+    l1distance (a,b) (c,d) = abs (a-c) + abs (b-d)
+    properLocation k = (div k n, mod k n)
+    actualLocation k = fromJust $ lookup k $ assocs_arr
+    assocs_arr = concat $ map (\(r,z) -> [(k,(r,c)) | (c,k) <- assocs z]) $
+                 assocs arr
+
+gaschnigDistance :: SlidingBlockPuzzle -> Int
+gaschnigDistance p@(SBP n r c d)
+  | d_list == [0..n^2-1] = 0
+  | otherwise = 1 + gaschnigDistance (swap (r',c'))
+    where
+      d_list = concat (map elems (elems d))
+      badIndex = bi (tail d_list) 1
+      bi (x:xs) y
+        | x == y = bi xs (y+1)
+        | otherwise = y
+      zzpos = fromJust $ elemIndex (n*r+c) d_list
+      (r',c')
+        | r == 0 && c == 0 = (div badIndex n,mod badIndex n)
+        | otherwise = (div zzpos n,mod zzpos n)
+      swap (x,y)
+        | x == r = SBP n x y $ d // [(r,(d!r) // [(y,0),(c,(d!r)!y)])]
+        | otherwise = SBP n x y $ d // [(r,(d!r) // [(c,(d!x)!y)]),
+                                        (x,(d!x) // [(y,0)])]
+
+main :: IO ()
+main = do
+  let x1 = SBP 2 0 0 $ listArray (0,1) $ map (listArray (0,1)) [[0,3],[1,2]]
+  putStrLn "2x2 puzzle with breadth first search:"
+  print $ breadthFirstTreeSearch {-(map (\z -> (z,1)) .-}( expandSlidingBlockPuzzle) x1 (==solvedSlidingBlockPuzzle 2)
+
+  let x2 = SBP 3 1 2 $ listArray (0,2) $ map (listArray (0,2)) [[3,4,7],[5,1,0],[6,8,2]]
+  putStrLn "3x3 puzzle with A* search using misplaced distance:"
+  print $ aStarGraphSearch (map (\z -> (z,1)) . expandSlidingBlockPuzzle) misplacedDistance x2 (==solvedSlidingBlockPuzzle 3)
+  putStrLn "3x3 puzzle with A* search using Gaschnig distance:"
+  print $ aStarGraphSearch (map (\z -> (z,1)) . expandSlidingBlockPuzzle) gaschnigDistance x2 (==solvedSlidingBlockPuzzle 3)
+  putStrLn "3x3 puzzle with A* search using Manhattan distance:"
+  print $ aStarGraphSearch (map (\z -> (z,1)) . expandSlidingBlockPuzzle) manhattanDistance x2 (==solvedSlidingBlockPuzzle 3)
+
+  putStrLn "4x4 puzzle using max of Manhattan & Gaschnig distances (slow):"
+  let x3 = SBP 4 1 3 $ listArray (0,3) $ map (listArray (0,3)) [[3,10,4,7],[13,5,1,0],[15,11,9,14],[12,6,8,2]]
+  print $ aStarGraphSearch (map (\z -> (z,1)) . expandSlidingBlockPuzzle) (\z -> max (manhattanDistance z) (gaschnigDistance z)) x3 (==solvedSlidingBlockPuzzle 4)
